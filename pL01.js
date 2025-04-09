3967bdace2f88a2b231d90527654dcdc8a4ecd6e
mungsil <thddmd2009@gmail.com>
2025-04-09 10:47:00 +0900

    형 변환

--- pL01.js
+++ pL01.js
@@ -46,3 +46,19 @@ let empty = null;
 
 // 5) Undefined
 let none; // undefined는 변수가 선언되었지만 값이 할당되지 않은 상태를 나타냄
+
+// ===========================================
+
+// 형 변환
+// 1. 묵시적
+let num = 10;
+let strNum1 = "20";
+let result = num + strNum1; //1020
+
+// 2. 명시적 - 내장함수 등을 이용해서 직접 형 변환을 명시
+let strToNum1 = Number(strNum1);
+console.log(strToNum1); // 20
+
+let strNum2 = "200개";
+console.log(Number(strNum2)); // NaN
+console.log(parseInt(strNum2)); // 20, parseInt: 순수 숫자로 이루어져있지 않은 문자열도 정상적 변환, 단 첫 글자가 숫자가 아니면 NaN
