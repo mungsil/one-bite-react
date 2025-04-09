872c6e5b21dad911f43476813be032398cd91039
mungsil <thddmd2009@gmail.com>
2025-04-09 10:15:19 +0900

    변수, 상수

---
+++ pL01.js
@@ -0,0 +1,17 @@
+// 변수
+let age;
+age = 30;
+age = 30.5;
+
+// 상수
+const birthYear = 1991; // birthYear is a constant and cannot be reassigned
+
+// 명명 규칙
+// $, _ 기호만 사용 가능
+let _name = "John";
+
+// 숫자 시작 불가
+// let 1stName = "John"; // Invalid variable name
+
+// 예약어 불가
+// let class = "Math"; // Invalid variable name
