c5621181cf55924fe194df08eb8d00cbda9fa3d4
mungsil <thddmd2009@gmail.com>
2025-04-12 16:20:35 +0900

    라이브러리리

--- sec03/src/index.js
+++ sec03/src/index.js
@@ -1,6 +1,9 @@
-import multiply, { add, sub } from "./math.js";
+import randomcolor from "randomcolor";
 
-console.log("안녕 Node.js");
-console.log("더하기" + add(1, 2));
-console.log("빼기" + sub(1, 2));
-console.log("곱하기" + multiply(1, 2));
+const color = randomcolor();
+console.log(color);
+
+// console.log("안녕 Node.js");
+// console.log("더하기" + add(1, 2));
+// console.log("빼기" + sub(1, 2));
+// console.log("곱하기" + multiply(1, 2));
