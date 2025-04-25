da077d655f98208e89f751d838b72b64d3a816cd
mungsil <thddmd2009@gmail.com>
2025-04-25 13:47:27 +0900

    event handling

--- sec05/src/components/Button.jsx
+++ sec05/src/components/Button.jsx
@@ -1,6 +1,19 @@
 const Button = ({ children, text, color = "blue" }) => {
+  // 이벤트 객체
+  function handleClick(e) {
+    // SyntheticBaseEvent : React에서 제공하는 이벤트 객체
+    // SyntheticBaseEvent는 브라우저에 따라 다르게 동작하는 이벤트를 통일된 인터페이스로 제공한다.
+    // Synthetic=합성
+    console.log("버튼 클릭", e);
+    // alert("버튼 클릭");
+  }
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={handleClick}
+      // onMouseEnter={handleMouseEnter}
+      style={{ color: color }}
+    >
       {text}
       {children}
     </button>
