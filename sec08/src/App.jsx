e2b58e9c4880bbdbcd4a0f0eec573b229c467131
mungsil <thddmd2009@gmail.com>
2025-05-08 17:19:42 +0900

    옵션 생성 구현

--- sec08/src/App.jsx
+++ sec08/src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
+import { useState, useRef } from "react";
+
 import Header from "./components/header/Header";
 import List from "./components/list/List";
 import Editor from "./components/editor/Editor";
 
+const mockData = [
+  { id: 0, content: "동태찜", date: new Date().toDateString(), isClear: false },
+  {
+    id: 1,
+    content: "바닐라 라떼",
+    date: new Date().toDateString(),
+    isClear: true,
+  },
+];
+
 function App() {
+  const [items, setItems] = useState(mockData);
+  const idRef = useRef(2);
+
+  const handleAddItem = (newContent) => {
+    const newItem = {
+      id: idRef.current++,
+      content: newContent,
+      date: new Date().toTimeString(),
+      isCompleted: false,
+    };
+    setItems([newItem, ...items]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onSubmit={handleAddItem} />
       <List />
     </div>
   );
