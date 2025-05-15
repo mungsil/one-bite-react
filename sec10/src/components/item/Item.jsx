c16cf6e5bf60ab24f98754ca0a93b62a20021251
mungsil <thddmd2009@gmail.com>
2025-05-15 17:29:27 +0900

    일관된 변수명으로 수정

--- sec10/src/components/item/Item.jsx
+++ sec10/src/components/item/Item.jsx
@@ -1,13 +1,9 @@
 import "./Item.css";
 
-const Item = ({ id, content, date, isCompleted, onCheck, onDelete }) => {
+const Item = ({ id, content, date, isClear, onCheck, onDelete }) => {
   return (
     <div className="Item">
-      <input
-        checked={isCompleted}
-        type="checkbox"
-        onChange={() => onCheck(id)}
-      />
+      <input checked={isClear} type="checkbox" onChange={() => onCheck(id)} />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button onClick={() => onDelete(id)}>삭제</button>
