602a36845777a72afed5ea917d46f7d4b0314b44
mungsil <thddmd2009@gmail.com>
2025-05-18 21:45:32 +0900

    Link, useNavigate를 이용한 라우팅

--- sec12/src/App.jsx
+++ sec12/src/App.jsx
@@ -3,12 +3,27 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/NotFound";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import { use } from "react";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickBtn = () => {
+    // useNavigate 훅을 사용하여 페이지 이동, 이벤트 핸들러에서 사용
+    nav("/new");
+  };
+
   return (
     <>
-      <h1>안녕안녕안녕하세요 </h1>
+      {/* html a 태그 -> CSR 불가, Link 컴포넌트를 쓰자 */}
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/notfound"}>NotFound</Link>
+      </div>
+      <button onClick={onClickBtn}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
