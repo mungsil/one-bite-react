f1a156d4cd2860f5a4896f9be6ce6786a28aa412
mungsil <thddmd2009@gmail.com>
2025-05-21 21:38:13 +0900

    폰트, 이미지, 레이아웃 설정 실습

--- sec12/src/App.jsx
+++ sec12/src/App.jsx
@@ -4,7 +4,8 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/NotFound";
 import { Route, Routes, Link, useNavigate } from "react-router-dom";
-import { use } from "react";
+
+import { getEmotionImage } from "./util/get-emotion-image";
 
 function App() {
   const nav = useNavigate();
@@ -16,6 +17,19 @@ function App() {
 
   return (
     <>
+      {/* 이미지를 public 폴더에 넣고, import가 아닌 경로로 이미지를 불러올 수도 있다.
+        해당 방법은 vite가 자동으로 제공하는 이미지 최적화가 동작하지 않는다. 
+        - 최적화 -> 브라우저에 이미지 캐싱
+        - 소수의 이미지 -> import
+        - 많은 이미지 -> public 폴더에 넣고 경로로 불러오기 추천~
+        */}
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       {/* html a 태그 -> CSR 불가, Link 컴포넌트를 쓰자 */}
       <div>
         <Link to={"/"}>Home</Link>
