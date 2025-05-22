022e67f532fd308f13a0bb24395e6cebb3c9f2f4
mungsil <thddmd2009@gmail.com>
2025-05-22 09:57:21 +0900

    버튼 컴포넌트 구현

--- sec12/src/App.jsx
+++ sec12/src/App.jsx
@@ -4,6 +4,7 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/NotFound";
 import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Button from "./components/Button";
 
 import { getEmotionImage } from "./util/get-emotion-image";
 
@@ -17,27 +18,27 @@ function App() {
 
   return (
     <>
-      {/* 이미지를 public 폴더에 넣고, import가 아닌 경로로 이미지를 불러올 수도 있다.
-        해당 방법은 vite가 자동으로 제공하는 이미지 최적화가 동작하지 않는다. 
-        - 최적화 -> 브라우저에 이미지 캐싱
-        - 소수의 이미지 -> import
-        - 많은 이미지 -> public 폴더에 넣고 경로로 불러오기 추천~
-        */}
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      {/* html a 태그 -> CSR 불가, Link 컴포넌트를 쓰자 */}
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <Link to={"/notfound"}>NotFound</Link>
-      </div>
-      <button onClick={onClickBtn}>New 페이지로 이동</button>
+      <Button
+        text={"버튼1"}
+        onClick={() => {
+          console.log("버튼1 클릭");
+        }}
+        type={"DEFAULT"}
+      ></Button>
+      <Button
+        text={"버튼2"}
+        onClick={() => {
+          console.log("버튼2 클릭");
+        }}
+        type={"POSITIVE"}
+      ></Button>
+      <Button
+        text={"버튼3"}
+        onClick={() => {
+          console.log("버튼3 클릭");
+        }}
+        type={"NEGATIVE"}
+      ></Button>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
